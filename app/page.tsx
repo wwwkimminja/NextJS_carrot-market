8f6eafb432fbad165d0c4662d92ac287266e1ad4
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-04-04 20:15:28 +0900

    modifier

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-2xl w-full">
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-500">
         <div className="flex justify-between items-center">
           <div className="flex flex-col">
-            <span className="text-gray-600 -mb-1">In transit</span>
-            <span className="text-4xl font-semibold ">Coolblue</span>
+            <span className="text-gray-600 -mb-1 dark:text-gray-300">In transit</span>
+            <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
           </div>
           <div className="size-12 bg-orange-400 rounded-full"/>
         </div>
           <div className="my-2 flex items-center gap-2">
-            <span className="bg-green-400 text-white uppercase rounded-full px-3 py-1.5 text-xs font-medium">Today</span>
-            <span>9:30-10:00</span>
+            <span className="bg-green-400 text-white uppercase rounded-full px-3 py-1.5 text-xs font-medium transition hover:bg-green-500 hover:scale-125">Today</span>
+            <span className=" dark:text-gray-100">9:30-10:00</span>
           </div>
           <div className="relative">
             <div className="bg-gray-200 absolute w-full h-2 rounded-full"/>
             <div className="bg-green-400 absolute w-2/3 h-2 rounded-full"/>
           </div>
-          <div className=" flex justify-between items-center mt-5 text-gray-600">
+          <div className=" flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
             <span>Expected</span>
             <span>Sorting center</span>
             <span>In transit</span>
-            <span className=" text-gray-400">Delivered</span>
+            <span className=" text-gray-400 dark:text-gray-600">Delivered</span>
           </div>
       </div>
     </main>
