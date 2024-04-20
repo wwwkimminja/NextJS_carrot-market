0afabe6f7acc074b48cb59fe2c535a9f57a4876c
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-04-20 16:48:36 +0900

    state modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex flex-col gap-5 items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full 
+      max-w-screen-sm flex flex-col md:flex-row gap-2 
+      *:outline-none ring ring-transparent transition-shadow has-[.peer]:bg-green-100 has-[:invalid]:bg-red-100 has-[:invalid]:ring-red-400">
         <input className="w-full rounded-full
-        bg-gray-200 pl-5 py-3 outline-none ring ring-transparent
+        bg-gray-200 pl-5 py-3 ring ring-transparent
         focus:ring-blue-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow
         invalid:focus:ring-red-500 peer" type="email" required placeholder="Email address" />
           <span className="text-red-500 font-medium hidden peer-invalid:block ">
           Email is required.
         </span>
         <button className="text-white py-2 rounded-full active:scale-90 
-        transition-transform font-medium outline-none md:px-10 bg-black peer-invalid:bg-red-200">Log in
+        transition-transform font-medium md:px-10 bg-black peer-invalid:bg-red-200">Log in
         </button>
       </div>
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-500">
