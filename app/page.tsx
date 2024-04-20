bb699f5f5ffdbd88859ba8aa0605475202a1e9c8
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-04-20 15:10:13 +0900

    responsive modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex flex-col items-center justify-center p-5 dark:bg-gray-700">
-       <div className="bg-white shadow-lg p-5 rounded-3xl w-full mb-3 max-w-screen-sm flex flex-col gap-5">
+    <main className="bg-gray-100 h-screen flex flex-col gap-5 items-center justify-center p-5 dark:bg-gray-700">
+       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
         <input className="w-full rounded-full bg-gray-200 pl-5 py-3 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow" type="text" placeholder="Search here..."/>
-        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">Search</button>
+        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10">Search</button>
        </div>
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-500">
         <div className="flex justify-between items-center">
