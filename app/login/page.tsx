831f7b187000bc0ed82c302e7ca364107f7ac96b
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-05-26 14:37:59 +0900

    feat : Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
 import React from "react";
+import FormInput from "../components/form-input";
+import FormBtn from "../components/form-btn";
+import SocialLogin from "../components/social-login-btn";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">Hello!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="email address" required={true} errors={[]} />
+        <FormInput type="password" placeholder="password" required={true} errors={[]} />
+        <FormBtn loading={false} text="Create Account" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
 }
