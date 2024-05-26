925fe08d283493fe883bf8d5e24d03be88978cb1
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-05-26 14:43:33 +0900

    feat : SMSLogin

---
+++ app/sms/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+import FormInput from "../components/form-input";
+import FormBtn from "../components/form-btn";
+import SocialLogin from "../components/social-login-btn";
+
+export default function SMSLogin() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">SMS Login</h1>
+        <h2 className="text-xl">Verify your phone number.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="number" placeholder="phone number" required={true} errors={[]} />
+        <FormInput type="number" placeholder="Verification Code" required={true} errors={[]} />
+        <FormBtn loading={false} text="Verify" />
+      </form>
+    </div>
+  );
+}
