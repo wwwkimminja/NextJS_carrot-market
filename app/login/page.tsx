c8756dfbf5c37c13b92bf471e56dcd4c320d1fff
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-06-01 17:36:17 +0900

    fix : Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,12 +4,11 @@ import Input from "../components/input";
 import Button from "../components/button";
 import SocialLogin from "../components/social-login-btn";
 import { useFormState } from "react-dom";
-import { handleForm } from "./actions";
+import { login } from "./actions";
+import { PASSWORD_MIN_LENGTH } from "../lib/constants";
 
 export default function Login() {
-  const [state, action] = useFormState(handleForm, {
-    potato: 1,
-  } as any);
+  const [state, action] = useFormState(login, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -17,9 +16,16 @@ export default function Login() {
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <Input name="email" type="email" placeholder="email address" required={true} errors={state.errors} />
-        <Input name="password" type="password" placeholder="password" required={true} errors={state.errors} />
-        <Button text="Create Account" />
+        <Input name="email" type="email" placeholder="email address" required errors={state?.fieldErrors.email} />
+        <Input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          errors={state?.fieldErrors.password}
+          minLength={PASSWORD_MIN_LENGTH}
+        />
+        <Button text="Login" />
       </form>
       <SocialLogin />
     </div>
